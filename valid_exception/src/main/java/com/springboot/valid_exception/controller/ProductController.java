aa84fc6b11f07a3fc72b719560ef5c63e95041c1
leetaehee <lastride25@naver.com>
2023-02-21 09:11:00 +0900

    수정사항 반영

---
+++ valid_exception/src/main/java/com/springboot/valid_exception/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.springboot.valid_exception.controller;
+
+import com.springboot.valid_exception.data.dto.ProductDto;
+import com.springboot.valid_exception.data.dto.ProductResponseDto;
+import com.springboot.valid_exception.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<ProductResponseDto> getProduct(Long number) {
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto) {
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+}
+
