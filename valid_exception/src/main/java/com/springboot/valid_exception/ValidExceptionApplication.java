2ec42555617818c437965b84fdc5a701d91bcfb6
leetaehee <lastride25@naver.com>
2023-02-21 09:23:24 +0900

    복구

--- valid_exception/src/main/java/com/springboot/valid_exception/ValidExceptionApplication.java
+++ valid_exception/src/main/java/com/springboot/valid_exception/ValidExceptionApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ValidExceptionApplication {
     public static void main(String[] args) {
-        //11
         SpringApplication.run(ValidExceptionApplication.class, args);
     }
 }
